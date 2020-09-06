16ab8ea9594f00ef57ae2a4870c32d695d0e973a
VladimirKasharevsky <kashar015@rambler.ru>
2020-09-06 20:14:02 +0300

    Add CrossOrigin

--- src/main/java/com/example/garden/controller/UserController.java
+++ src/main/java/com/example/garden/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.garden.entity.Response;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("user/")
 public class UserController {
